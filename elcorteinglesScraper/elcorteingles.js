49271fd63e9fda5a5f18be16b7ebd268103daf6c
Daniel Carrillo Cardús <danielcarrillocardus@gmail.com>
2021-04-18 17:30:05 +0200

    Improving El Corte Ingles's scraper.

--- elcorteinglesScraper/elcorteingles.js
+++ elcorteinglesScraper/elcorteingles.js
@@ -1,4 +1,3 @@
-//const puppeteer = require('puppeteer');
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
@@ -6,46 +5,58 @@ const fs = require('fs');
 const urls = require('./eciProducts.json');
 
 (async () => {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
-
-    const categories = Object.keys(urls);
-    const page = await browser.newPage();
-
-    let isFirstTime = true;
     for (let category in urls) {
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+
         const page = await browser.newPage();
         for (let subcategory in urls[category]) {
+            const products = [];
+            let actualPage;
+            let lastPage;
+            let isFirstTime;
             for (let url of urls[category][subcategory]) {
-                await page.goto(url);
-                const products = await getProducts(page, isFirstTime);
-                isFirstTime = false;
-                console.log(products);
+                actualPage = 1;
+                lastPage = 99;
+                isFirstTime = true;
 
-                /*
-                const dir = ./data/${category}/;
-                if (!fs.existsSync(dir)) {
-                    fs.mkdirSync(dir, {
-                        recursive: true
-                    });
+                while (actualPage <= lastPage) {
+                    await page.goto(`${url}${actualPage}/`);
+                    await page.waitForSelector('.grid-coincidences');
+
+                    if (isFirstTime) {
+                        lastPage = await page.evaluate(() => {
+                            const tmp = document.querySelector('.grid-coincidences').innerText;
+                            const tmpArray = tmp.split(' ');
+                            lastPage = parseInt(Math.ceil(tmpArray[0] / 24));
+                            return lastPage;
+                        });
+                        isFirstTime = false;
+                    }
+
+                    products.push(await getProducts(page));
+                    actualPage++;
                 }
-                fs.writeFile(./data/${category}/${subCategory}.json, JSON.stringify(products), err => {
-                    if (err) throw err;
-                });
-                */
             }
+
+            const dir = `./data/products/eci/${category}/`;
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir, {
+                    recursive: true
+                });
+            }
+            fs.writeFile(`./data/products/eci/${category}/${subcategory}.json`, JSON.stringify(products), err => {
+                if (err) throw err;
+            });
         }
         await page.close();
+        await browser.close();
     }
 })();
 
-async function getProducts(page, isFirstTime) {
-    await page.waitForTimeout(3000);
-    if (isFirstTime) {
-        await page.click('._pagination');
-    }
-    await autoScroll(page);
+async function getProducts(page) {
+    //await autoScroll(page);
 
     const elements = await page.evaluate(() =>
         [...document.querySelectorAll('.js-product')].map(elem => {
@@ -53,7 +64,9 @@ async function getProducts(page, isFirstTime) {
                 const name = JSON.parse(elem.dataset.json).name;
                 const brand = JSON.parse(elem.dataset.json).brand;
                 const price = JSON.parse(elem.dataset.json).price.final;
-                const img = elem.querySelector('.product_tile-left_container > .product_tile-image > a > img').src;
+                const img = changeImgSrc(
+                    elem.querySelector('.product_tile-left_container > .product_tile-image > a > img').src
+                );
                 const pack = extractPack(name);
                 const container = extractContainer(name);
 
@@ -77,25 +90,11 @@ async function getProducts(page, isFirstTime) {
                 }
                 return productContainer;
             }
+
+            function changeImgSrc(img) {
+                return img.replace('40x40', '325x325');
+            }
         })
     );
     return elements;
-}
-
-async function autoScroll(page) {
-    await page.evaluate(async () => {
-        await new Promise((resolve, reject) => {
-            let totalHeight = 0;
-            let distance = 50;
-            let timer = setInterval(() => {
-                let scrollHeight = document.body.scrollHeight;
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-                if (totalHeight >= scrollHeight - 1300) {
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 5);
-        });
-    });
 }
