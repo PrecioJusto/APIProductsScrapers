998f51051932f9e1e7d91fac60cf64ec7ba5b44d
Daniel Carrillo Cardús <danielcarrillocardus@gmail.com>
2021-06-09 20:46:55 +0200

    Refactored start.js

--- start.js
+++ start.js
@@ -5,20 +5,19 @@ const elcorteingles = require('./elcorteinglesScraper/elcorteingles');
 const hipercor = require('./hipercorScraper/hipercor');
 const productDecorator = require('./service/decorators/productDecorator');
 const request = require('./controller/request');
-//
 
-T//his script can take a total of 10 hours to fully run.
+//This script can take a total of 10 hours to fully run.
 (async () => {
     const al = alcampo.executeAlcampo();
     const el = elcorteingles.executeEci();
     const hi = hipercor.executeHipercor();
     const rc = recipesCarrefour.executeRecipes();
-    const promises = await Promise.all([al, el, hi, rc]);
-    const ca = await carrefour.executeCarrefour();
+    await Promise.all([al, el, hi, rc]);
+    await carrefour.executeCarrefour();
 
-    const pd = await productDecorator.decorator();
+    const data = await productDecorator.decorator();
     
     //Do a request with all products to APIProducts
-    const status = await request.sendProductData(pd);
+    const status = await request.sendProductData(data);
     await console.log(status);
 })();
