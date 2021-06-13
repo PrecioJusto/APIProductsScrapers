c3c83e1819b00285f97b172a3c77a669ec01558c
Daniel Carrillo Cardús <danielcarrillocardus@gmail.com>
2021-06-13 17:15:16 +0200

    commented scrapers.

--- start.js
+++ start.js
@@ -6,18 +6,18 @@ const hipercor = require('./hipercorScraper/hipercor');
 const productDecorator = require('./service/decorators/productDecorator');
 const request = require('./controller/request');
 
-//This script can take a total of 10 hours to fully run.
 (async () => {
-    const al = alcampo.executeAlcampo();
-    const el = elcorteingles.executeEci();
-    const hi = hipercor.executeHipercor();
-    const rc = recipesCarrefour.executeRecipes();
-    await Promise.all([al, el, hi, rc]);
-    await carrefour.executeCarrefour();
+    // If you want to test scrapers, decomment next commented lines.
+
+    // const al = alcampo.executeAlcampo();
+    // const el = elcorteingles.executeEci();
+    // const hi = hipercor.executeHipercor();
+    // const rc = recipesCarrefour.executeRecipes();
+    // await Promise.all([al, el, hi, rc]);
+    // await carrefour.executeCarrefour();
 
     const data = await productDecorator.decorator();
     
-    //Do a request with all products to APIProducts
     const status = await request.sendProductData(data);
     await console.log(status);
 })();
